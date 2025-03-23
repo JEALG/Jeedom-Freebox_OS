e365ee9f0685c470a40ab6c298e0d72ba8d584a5
JEALG <jalain.gosse@icloud.com>
2025-03-23 11:44:52 +0100

    clean

--- core/class/Free_API.class.php
+++ core/class/Free_API.class.php
@@ -185,7 +185,7 @@ class Free_API
             while ($session_token->getValue('') == '') {
                 $session_token = cache::byKey('Freebox_OS::SessionToken');
             }
-            $requetURL = '[Freebox Request Connexion] : ' . $method . ' ' . (__('sur la l\'adresse', __FILE__)) . ' ' . $this->serveur . $api_url . '(' . json_encode($params) . ')';
+            //$requetURL = '[Freebox Request Connexion] : ' . $method . ' ' . (__('sur la l\'adresse', __FILE__)) . ' ' . $this->serveur . $api_url . '(' . json_encode($params) . ')';
             $requetURL = '[Freebox Request Connexion] : ' . $method . ' ' . (__('sur la l\'adresse', __FILE__)) . ' ' . $this->serveur . $api_url  . json_encode($params);
             if ($log_request  != false) {
                 log::add('Freebox_OS', 'debug', $requetURL);
