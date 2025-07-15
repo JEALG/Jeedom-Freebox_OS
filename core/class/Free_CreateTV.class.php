93d114d5fc60391ec10684ca41be6f4b1034efaf
JEALG <jalain.gosse@icloud.com>
2025-07-15 08:48:56 +0200

    Pas de création la commande IP du player si mode bridge est actif

--- core/class/Free_CreateTV.class.php
+++ core/class/Free_CreateTV.class.php
@@ -126,7 +126,12 @@ class Free_CreateTV
                                 $EqLogic->AddCommand(__('Mute On', __FILE__), 'muteOn', 'action', 'other', 'core::toggleLine', null, 'SWITCH_ON', 1,  $Mute, 'mute', 0, $iconmuteon, 1, null, null, $order++, '0', true, 'never', null, true, null, null, null, null, null, null, null, null);
                                 $EqLogic->AddCommand(__('Mute Off', __FILE__), 'muteOff', 'action', 'other', 'core::toggleLine', null, 'SWITCH_OFF', 1,  $Mute, 'mute', 0, $iconmuteoff, 1, null, null, $order++, '0', true, 'never', null, true, null, null, null, null, null, null, null, null);
                                 $EqLogic->AddCommand(__('Redémarrage', __FILE__), 'reboot', 'action', 'other',  $templatecore_V4 . 'line', null, null, 1, 'default', 'default', 0, $iconReboot, true, 'default', 'default',   $order++, '0', true, null, null, true, null, null, null, null, null, null, true, null, null, null, null, null, null, null, null, null, null);
-                                $EqLogic->AddCommand(__('Adresse IPV4 du player', __FILE__), 'addr', 'info', 'string', null, null, null, 0, 'default', 'default', 0, null, 0, 'default', 'dafault', $order++, '0', false, false);
+                                if (config::byKey('TYPE_FREEBOX_MODE', 'Freebox_OS') == 'router') {
+                                    log::add('Freebox_OS', 'debug', '|:fg-info: ───▶︎ ' . (__('BOX EN MODE ROUTER : Création de la commande Adresse IPV4 du player', __FILE__)) . ':/fg:');
+                                    $EqLogic->AddCommand(__('Adresse IPV4 du player', __FILE__), 'addr', 'info', 'string', null, null, null, 0, 'default', 'default', 0, null, 0, 'default', 'dafault', $order++, '0', false, false);
+                                } else {
+                                    log::add('Freebox_OS', 'debug', '|:fg-warning: ───▶︎ ' . (__('BOX EN MODE BRIDGE : Pas de création de la commande Adresse IPV4 du player', __FILE__)) . ':/fg:');
+                                }
                             }
                         }
                     } else {
